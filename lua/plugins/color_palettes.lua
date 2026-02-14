534094e7b6795712c562ccda3a0dc2389b95518f
Aariyan Al Mamun <aariyan@gmail.com>
2026-02-14 18:27:40 -0500

    changed theme

--- lua/plugins/color_palettes.lua
+++ lua/plugins/color_palettes.lua
@@ -1,10 +1,10 @@
 return {
 	{
-		"ellisonleao/gruvbox.nvim",
+		"xero/miasma.nvim",
 		lazy = false,
 		priority = 1000,
 		config = function()
-			vim.cmd.colorscheme("gruvbox")
+			vim.cmd("colorscheme miasma")
 		end,
  	},
 }
